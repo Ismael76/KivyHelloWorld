73e0a4b979d4f9a084e6afc8944106fd923b0748
Ismael <IsmaelAli79@Outlook.Com>
2021-03-13 21:14:21 +0000

    TEST

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.button import Button
+from kivy.uix.screenmanager import Screen
 
+class HomeScreen(Screen):
+    pass
+
+class SettingScreen(Screen):
+    pass
+
+
+GUI = Builder.load_file("main.kv")
 
 class MainApp(App):
     def build(self):
-        return Button(text="Hello World")
+        return GUI
 
+
+    def change_screen(self, screen_name):
+        #Use 'screen_manager' to do this
+        screen_manager = self.root.ids['screen_manager']
+        screen_manager.current = screen_name
 
 MainApp().run()
